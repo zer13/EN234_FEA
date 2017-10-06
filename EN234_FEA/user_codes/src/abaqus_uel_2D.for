082e0775c15e12c341f23f15634308d8eef40ac4
zer13 <32487726+zer13@users.noreply.github.com>
2017-10-05 20:20:00 -0400

    Add files via upload

--- EN234_FEA/user_codes/src/abaqus_uel_2D.for
+++ EN234_FEA/user_codes/src/abaqus_uel_2D.for
@@ -11,7 +11,7 @@
 !          abq_UEL_1D_integrationpoints(n_points, n_nodes, xi, w)  = defines integration points for 1D line integral
 !=========================== ABAQUS format user element subroutine ===================
 
-      SUBROUTINE UEL_2D(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
+      SUBROUTINE UEL(RHS,AMATRX,SVARS,ENERGY,NDOFEL,NRHS,NSVARS,
      1     PROPS,NPROPS,COORDS,MCRD,NNODE,U,DU,V,A,JTYPE,TIME,DTIME,
      2     KSTEP,KINC,JELEM,PARAMS,NDLOAD,JDLTYP,ADLMAG,PREDEF,NPREDF,
      3     LFLAGS,MLVARX,DDLMAG,MDLOAD,PNEWDT,JPROPS,NJPROP,PERIOD)
@@ -105,10 +105,10 @@
 
     !   Variables below are for computing integrals over element faces
       double precision  ::  face_coords(2,3)                  ! Coords of nodes on an element face
-      double precision  ::  xi1(6)                            ! 1D integration points
+      double precision  ::  xi1(1,6)                            ! 1D integration points
       double precision  ::  w1(6)                              ! Integration weights
       double precision  ::  N1(3)                             ! 1D shape functions
-      double precision  ::  dN1dxi(3)                         ! 1D shape function derivatives
+      double precision  ::  dNdxi1(3,1)                         ! 1D shape function derivatives
       double precision  ::  norm(2)                           ! Normal to an element face
       double precision  ::  dxdxi1(2)                         ! Derivative of 1D spatial coord wrt normalized areal coord
     !
@@ -143,7 +143,80 @@
 
     ! Write your code for a 2D element below
 
-      END SUBROUTINE UEL_2D
+
+
+
+      call abq_UEL_2D_integrationpoints(n_points, NNODE, xi, w)
+
+      if (MLVARX<2*NNODE) then
+        write(6,*) ' Error in abaqus UEL '
+        write(6,*) ' Variable MLVARX must exceed 2*NNODE'
+        write(6,*) ' MLVARX = ',MLVARX,' NNODE = ',NNODE
+        stop
+      endif
+
+      RHS(1:MLVARX,1) = 0.d0
+      AMATRX(1:NDOFEL,1:NDOFEL) = 0.d0
+
+      D = 0.d0
+      E = PROPS(1)
+      xnu = PROPS(2)
+      d44 = 0.5D0*E/(1+xnu)
+      d11 = (1.D0-xnu)*E/( (1+xnu)*(1-2.D0*xnu) )
+      d12 = xnu*E/( (1+xnu)*(1-2.D0*xnu) )
+      D(1:3,1:3) = d12
+      D(1,1) = d11
+      D(2,2) = d11
+      D(3,3) = d11
+      D(4,4) = d44
+
+      ENERGY(1:8) = 0.d0
+
+    !     --  Loop over integration points
+      do kint = 1, n_points
+        call abq_UEL_2D_shapefunctions(xi(1:2,kint),NNODE,N,dNdxi)
+        dxdxi = matmul(coords(1:2,1:NNODE),dNdxi(1:NNODE,1:2))
+        call abq_UEL_invert2d(dxdxi,dxidx,determinant)
+        dNdx(1:NNODE,1:2) = matmul(dNdxi(1:NNODE,1:2),dxidx)
+        B = 0.d0
+        B(1,1:2*NNODE-1:2) = dNdx(1:NNODE,1)
+        B(2,2:2*NNODE:2) = dNdx(1:NNODE,2)
+        B(4,1:2*NNODE-1:2) = dNdx(1:NNODE,2)
+        B(4,2:2*NNODE:2) = dNdx(1:NNODE,1)
+        !B(3,:)=0
+
+        strain = matmul(B(1:4,1:2*NNODE),U(1:2*NNODE))
+
+        stress = matmul(D,strain)
+        RHS(1:2*NNODE,1) = RHS(1:2*NNODE,1)
+     1   - matmul(transpose(B(1:4,1:2*NNODE)),stress(1:4))*
+     2                                          w(kint)*determinant
+
+        AMATRX(1:2*NNODE,1:2*NNODE) = AMATRX(1:2*NNODE,1:2*NNODE)
+     1  + matmul(transpose(B(1:4,1:2*NNODE)),matmul(D,B(1:4,1:2*NNODE)))
+     2                                             *w(kint)*determinant
+
+        ENERGY(2) = ENERGY(2)
+     1   + 0.5D0*dot_product(stress,strain)*w(kint)*determinant           ! Store the elastic strain energy
+
+        if (NSVARS>=n_points*4) then   ! Store stress at each integration point (if space was allocated to do so)
+            SVARS(4*kint-3:4*kint) = stress(1:4)
+        endif
+      end do
+
+
+      PNEWDT = 1.d0          ! This leaves the timestep unchanged (ABAQUS will use its own algorithm to determine DTIME)
+    !
+    !   Apply distributed loads
+    !
+    !   Distributed loads are specified in the input file using the Un option in the input file.
+    !   n specifies the face number, following the ABAQUS convention
+    !
+    !   DELETED BECAUSE HW3 DOES NOT REQUIRE THIS.
+
+      return
+
+      END SUBROUTINE UEL
 
 
 
@@ -619,4 +692,31 @@
 
       end subroutine abq_inverse_LU
 
+
+      subroutine abq_UEL_invert2d(A,A_inverse,determinant)
+
+      double precision, intent(in) :: A(2,2)
+      double precision, intent(out) :: A_inverse(2,2)
+      double precision, intent(out) :: determinant
+
+      double precision COFACTOR(2,2)
+
+!   Compute inverse and determinant of 3x3 matrix
+
+      determinant =   A(1,1)*A(2,2)-A(1,2)*A(2,1)
+
+      IF (determinant==0.d0) THEN
+        write(6,*) ' Error in subroutine abq_UEL_invert2d'
+        write(6,*) ' A 2x2 matrix has a zero determinant'
+        stop
+      endif
+      COFACTOR(1,1) = +A(2,2)
+      COFACTOR(1,2) = -A(2,1)
+      COFACTOR(2,1) = -A(1,2)
+      COFACTOR(2,2) = +A(1,1)
+
+      A_inverse = transpose(COFACTOR) / determinant
+
+
+      end subroutine abq_UEL_invert2d
 
